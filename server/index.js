50264414b5da7c2f8df21c54fb95e101851e0ac1
Sam <36157647+oxedom@users.noreply.github.com>
2022-03-10 15:16:44 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const creds = require('./config/config')
+
+mongoose.connect
+(`mongodb+srv://${creds.username}:${creds.password}
+@cluster0.9261n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
