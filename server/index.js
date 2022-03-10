ae5b1386fbf7aef089b72b8e8370a8c0e2c71e78
Sam <36157647+oxedom@users.noreply.github.com>
2022-03-10 17:35:29 +0200

    POST works

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const app = express()
 const cors = require('cors')
 const config = require('./config/config')
 const user_routes = require('./routes/users')
+const User = require('./models/userModel')
 
 
 
-mongoose.connect(config)
+mongoose.connect(config, { useUnifiedTopology: true, useNewUrlParser: true } )
 .then( (result) => { app.listen(8000, console.log('Server is Online PORT 8000'))})
+const db = mongoose.connection 
+db.on("error", console.error.bind(console, "mongo connection error"))
+
+
+
+
+
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/user/', user_routes)
