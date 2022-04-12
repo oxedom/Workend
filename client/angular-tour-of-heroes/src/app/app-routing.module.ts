ca8e2bcc0b748194f6cc282472faf17e0b7466fc
Sam <36157647+oxedom@users.noreply.github.com>
2022-04-12 15:33:23 +0300

    kevin is cool

--- client/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ client/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './comps/feed/feed.component';
+import { LoginComponent } from './comps/login/login.component';
 import { ProfileComponent } from './comps/profile/profile.component';
+import { ProtectedComponentComponent } from './comps/protected-component/protected-component.component';
+import { RegisterComponent } from './comps/register/register.component';
 
 
-const routes: Routes = [   { path: 'feed', component: FeedComponent}, 
-{ path: 'profile', component: ProfileComponent }
+const routes: Routes = [
+{ path: 'feed', component: FeedComponent}, 
+{ path: 'profile', component: ProfileComponent },
+{ path: 'login', component: LoginComponent },
+{ path: 'register', component: RegisterComponent },
+{ path: 'protected', component: ProtectedComponentComponent }
+
 ];
 
+
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ client/angular-tour-of-heroes/src/app/service/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import * as moment from "moment"
+
+@Injectable()
+export class AuthService {
+
+    constructor() {}
+          
+    setLocalStorage(responseObj: { expiresIn: string; token: string; }) {
+
+        // Adds the expiration time defined on the JWT to the current moment
+        const expiresAt = moment().add(Number.parseInt(responseObj.expiresIn), 'days');
+
+        localStorage.setItem('id_token', responseObj.token);
+        localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+    }          
+
+    logout() {
+        localStorage.removeItem("id_token");
+        localStorage.removeItem("expires_at");
+    }
+
+    public isLoggedIn() {
+        return moment().isBefore(this.getExpiration(), "second");
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem("expires_at");
+        if (expiration) {
+            const expiresAt = JSON.parse(expiration);
+            return moment(expiresAt);
+        } else {
+            return moment();
+        }
+    }    
+}
