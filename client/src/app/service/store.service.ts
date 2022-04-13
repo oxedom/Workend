926760f966b57eb20a2b20858dfd1d7361b2ad1e
Sam <36157647+oxedom@users.noreply.github.com>
2022-04-13 10:29:18 +0300

    removed error next from usermodel

--- client/src/app/service/store.service.ts
+++ client/src/app/service/store.service.ts
@@ -9,6 +9,8 @@ export class StoreService {
   private mode$ = new BehaviorSubject<boolean>(true);
   currentMode$ = this.mode$.asObservable()
 
+  
+
   // private userid$ = new BehaviorSubject<string>('62376560612a0fbaeadca6b3');
   // currentuserid$ = this.userid$.asObservable()
 
